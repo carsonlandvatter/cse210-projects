a3a38618ee08b9ebe89daf37a3181bc8721ec8b8
carsonlandvatter <landvattercarson@gmail.com>
2025-03-12 22:38:21 -0600

    Completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        string playAgain;
+
+        do
+        {
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("Guess the magic number: ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else 
+                {
+                    Console.WriteLine($"You guessed it! It took you {guessCount} guesses!");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }

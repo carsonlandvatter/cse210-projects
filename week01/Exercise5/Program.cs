25d817fe3efd9ae14122d3f4bee4f503f78daef9
carsonlandvatter <landvattercarson@gmail.com>
2025-03-14 11:09:29 -0600

    Completed Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        
+        DisplayResult(userName, squaredNumber);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            string number = Console.ReadLine();
+            int userNumber = int.Parse(number);
+
+            return userNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int numberSquared = number * number;
+
+            return numberSquared;
+        }
+
+        static void DisplayResult(string name, int number)
+        {
+            Console.WriteLine($"{name}, the square of your number is {number}");
+        }
+
+
     }
 }
